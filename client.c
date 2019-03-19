a39fff88f871b757d24c3cb92e1988c87330bc9d
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 00:17:40 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,58 +1,73 @@
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8080 
-int main(int argc, char const *argv[]) 
-{ 
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    return 0; 
-} 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 3490
+#define MAXSIZE 1024
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in server_info;
+    struct hostent *he;
+    int socket_fd,num;
+    char buffer[1024];
+
+    char buff[1024];
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: client hostname\n");
+        exit(1);
+    }
+
+    if ((he = gethostbyname(argv[1]))==NULL) {
+        fprintf(stderr, "Cannot get host name\n");
+        exit(1);
+    }
+
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0))== -1) {
+        fprintf(stderr, "Socket Failure!!\n");
+        exit(1);
+    }
+
+    memset(&server_info, 0, sizeof(server_info));
+    server_info.sin_family = AF_INET;
+    server_info.sin_port = htons(PORT);
+    server_info.sin_addr = *((struct in_addr *)he->h_addr);
+    if (connect(socket_fd, (struct sockaddr *)&server_info, sizeof(struct sockaddr))<0) {
+        //fprintf(stderr, "Connection Failure\n");
+        perror("connect");
+        exit(1);
+    }
+
+    //buffer = "Hello World!! Lets have fun\n";
+    //memset(buffer, 0 , sizeof(buffer));
+    while(1) {
+        fgets(buffer,MAXSIZE-1,stdin);
+        if ((send(socket_fd,buffer, strlen(buffer),0))== -1) {
+            fprintf(stderr, "Failure Sending Message\n");
+            close(socket_fd);
+            exit(1);
+        }
+        else {
+            printf("Message being sent: %s\n",buffer);
+        }
+    }   
+
+    /*if ((num = recv(socket_fd, buff, 1024,0))== -1) {
+            //fprintf(stderr,"Error in receiving message!!\n");
+            perror("recv");
+            exit(1);
+        }   
+    //  num = recv(client_fd, buffer, sizeof(buffer),0);
+    buff[num] = '\0';
+    printf("Message received: %s\nNumber of bytes received: %d\n", buff,num);*/
+    close(socket_fd);   
+
+}
