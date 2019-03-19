fc3f54f3e716cc617da219217f83ec93f168891b
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 00:52:04 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,73 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <string.h>
 
-#define PORT 3490
-#define MAXSIZE 1024
+int main(){
+  int clientSocket;
+  char buffer[1024];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
 
-int main(int argc, char *argv[])
-{
-    struct sockaddr_in server_info;
-    struct hostent *he;
-    int socket_fd,num;
-    char buffer[1024];
+  /*---- Create the socket. The three arguments are: ----*/
+  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  
+  /*---- Configure settings of the server address struct ----*/
+  /* Address family = Internet */
+  serverAddr.sin_family = AF_INET;
+  /* Set port number, using htons function to use proper byte order */
+  serverAddr.sin_port = htons(7891);
+  /* Set IP address to localhost */
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  /* Set all bits of the padding field to 0 */
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-    char buff[1024];
+  /*---- Connect the socket to the server using the address struct ----*/
+  addr_size = sizeof serverAddr;
+  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: client hostname\n");
-        exit(1);
-    }
+  /*---- Read the message from the server into the buffer ----*/
+  recv(clientSocket, buffer, 1024, 0);
 
-    if ((he = gethostbyname(argv[1]))==NULL) {
-        fprintf(stderr, "Cannot get host name\n");
-        exit(1);
-    }
+  /*---- Print the received message ----*/
+  printf("Data received: %s",buffer);   
 
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0))== -1) {
-        fprintf(stderr, "Socket Failure!!\n");
-        exit(1);
-    }
-
-    memset(&server_info, 0, sizeof(server_info));
-    server_info.sin_family = AF_INET;
-    server_info.sin_port = htons(PORT);
-    server_info.sin_addr = *((struct in_addr *)he->h_addr);
-    if (connect(socket_fd, (struct sockaddr *)&server_info, sizeof(struct sockaddr))<0) {
-        //fprintf(stderr, "Connection Failure\n");
-        perror("connect");
-        exit(1);
-    }
-
-    //buffer = "Hello World!! Lets have fun\n";
-    //memset(buffer, 0 , sizeof(buffer));
-    while(1) {
-        fgets(buffer,MAXSIZE-1,stdin);
-        if ((send(socket_fd,buffer, strlen(buffer),0))== -1) {
-            fprintf(stderr, "Failure Sending Message\n");
-            close(socket_fd);
-            exit(1);
-        }
-        else {
-            printf("Message being sent: %s\n",buffer);
-        }
-    }   
-
-    /*if ((num = recv(socket_fd, buff, 1024,0))== -1) {
-            //fprintf(stderr,"Error in receiving message!!\n");
-            perror("recv");
-            exit(1);
-        }   
-    //  num = recv(client_fd, buffer, sizeof(buffer),0);
-    buff[num] = '\0';
-    printf("Message received: %s\nNumber of bytes received: %d\n", buff,num);*/
-    close(socket_fd);   
-
+  return 0;
 }
