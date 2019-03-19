52c2c480875e2fed50923b8b6865224a01e3e753
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-19 21:27:06 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,73 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <netdb.h>
-
-void error(char *msg) 
-{
-    perror(msg);
-    exit(0);
-}
-
-int main(int argc, char *argv[])
-{
-    int sockfd, portno;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    int n;
-    char buffer[256];
-
-    if (argc < 3)
-    {
-        error("ERROR, no port provided");
-    }
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0)
-    {
-        error("ERROR opening socket");
-    }
-
-    server = gethostbyname(argv[1]);
-
-    if (server == NULL)
-    {
-        error("ERROR, host not found\n");
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[2]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portno);
-
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-
-    if (connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        error("ERROR connecting to server");
-    }
-
-    printf("Enter a message for the server: ");
-
-    bzero(buffer,256);
-    fgets(buffer,sizeof(buffer),stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-
-    if (n < 0)
-    {/
-        error("ERROR writing to socket");
-    }
-
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-
-    if (n < 0)
-    {
-        error("ERROR reading from socket");
-    }
-
-    printf("%s\n", buffer);
-    return 0;
-}
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 8080 
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+    return 0; 
+} 
