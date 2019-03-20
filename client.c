8e0271e3de8ad4da7a7ee15997e44d1a46f0382d
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 08:35:12 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,68 +1,59 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-
-//#define ServerIP "35.162.226.229"
-#define ServerIP "127.0.0.1"
-#define ServerPort 7777
-#define SIZE 1024
-
-
-
-
-
-int main(int argc, char** argv) {
-
-    int socket_fd;
-    struct sockaddr_in serverAddr;
-    char text[SIZE],msg[SIZE-20], name[20]text temizlendi,get_msg[1024];
-    int byte;
-    // Nick
-    printf("Please enter your nick"); 
-    scanf("%s",name);
-    //Create Socket
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_fd == -1)
-        perror("Error on Socket");
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(ServerPort);
-    serverAddr.sin_addr.s_addr =inet_addr(ServerIP);
-    memset(&(serverAddr.sin_zero), '\0', 8);
-
-    // Connect to server
-    if(connect(socket_fd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr)) == -1)
-        perror("Error on Connect");
-    puts("Connected");
-    while(1)
-    {
-        //Get message
-        printf(">>>"); scanf("%s",msg);
-        //Edit message
-        strcpy(text, name);
-        strcat(text, " : ");
-        strcat(text, msg);
-        //Send message
-        byte = send(socket_fd, text, strlen(text), 0);
-        if(byte == -1)
-            perror("Error on Send");
-        else if(byte == 0)
-            printf("Connection've been closed");
-        //Get reply to other users
-
-        recv(socket_fd, &get_msg, SIZE-1, 0);
-        printf("Getting message %s", get_msg);
-    }
-    //Close socket
-    close(socket_fd);
-
-
-
-    return (EXIT_SUCCESS);
+#include<stdio.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<stdlib.h>
+#define MAX 80
+#define PORT 43454
+#define SA struct sockaddr
+void func(int sockfd)
+{
+char buff[MAX];
+int n;
+for(;;)
+{
+bzero(buff,sizeof(buff));
+printf("Enter the string : ");
+n=0;
+while((buff[n++]=getchar())!='\n');
+write(sockfd,buff,sizeof(buff));
+bzero(buff,sizeof(buff));
+read(sockfd,buff,sizeof(buff));
+printf("From Server : %s",buff);
+if((strncmp(buff,"exit",4))==0)
+{
+printf("Client Exit...\n");
+break;
 }
+}
+}
+
+int main()
+{
+int sockfd,connfd;
+struct sockaddr_in servaddr,cli;
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+if(sockfd==-1)
+{
+printf("socket creation failed...\n");
+exit(0);
+}
+else
+printf("Socket successfully created..\n");
+bzero(&servaddr,sizeof(servaddr));
+servaddr.sin_family=AF_INET;
+servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+servaddr.sin_port=htons(PORT);
+if(connect(sockfd,(SA *)&servaddr,sizeof(servaddr))!=0)
+{
+printf("connection with the server failed...\n");
+exit(0);
+}
+else
+printf("connected to the server..\n");
+func(sockfd);
+close(sockfd);
+}
+
