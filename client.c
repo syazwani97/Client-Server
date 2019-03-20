1c4d86d9e8c96b5a46a97b0f8f0fcc1e14d0dbd3
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 08:26:55 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,34 +1,91 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<string.h>
 
- 
-#define MAXRCVLEN 500
-#define PORTNUM 2343
- 
-int main(int argc, char *argv[])
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<sys/types.h>
+
+#include<sys/socket.h>
+
+#include<netinet/in.h>
+
+#include<errno.h>
+
+main()
+
 {
-    char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-    int len, mysocket;
-    struct sockaddr_in dest; 
- 
-    mysocket = socket(AF_INET, SOCK_STREAM, 0);
-  
-    memset(&dest, 0, sizeof(dest));                /* zero the struct */
-    dest.sin_family = AF_INET;
-    dest.sin_addr.s_addr = inet_addr("127.0.0.1"); /* set destination IP number */
-    dest.sin_port = htons(PORTNUM);                /* set destination port number */
- 
-    connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
-  
-    len = recv(mysocket, buffer, MAXRCVLEN, 0);
- 
-    /* We have to null terminate the received data ourselves */
-    buffer[len] = '\0';
- 
-    printf("Received %s (%d bytes).\n", buffer, len);
- 
-    closesocket(mysocket);
-    getch();
-    return EXIT_SUCCESS;
+
+            int sock, cli;
+
+            struct sockaddr_in server, clientDetail;
+
+            unsigned int len;
+
+            char mesg[] = “Hello world with Socket programming!”;
+
+            int sentconf;
+
+             if((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+
+            {
+
+                        perror(“There are some issue in creating socket : “);
+
+                        exit(-1);
+
+            }
+
+             server.sin_family = AF_INET;
+
+            server.sin_port = htons(10012);
+
+            server.sin_addr.s_addr = INADDR_ANY;
+
+            bzero(&server.sin_zero, 8);
+
+             len = sizeof(struct sockaddr_in);
+
+             if((bind(sock, (struct sockaddr *)&server, len)) == -1)
+
+            {
+
+                        perror(“Error in Binding”);
+
+                        exit(-1);
+
+            }
+
+             if((listen(sock, 5)) == -1)
+
+            {
+
+                        perror(“Error in Listening”);
+
+                        exit(-1);
+
+            }
+
+            while(1)
+
+            {
+
+                        if((cli = accept(sock, (struct sockaddr *)&clientDetail, &len)) ==-1)
+
+                        {
+
+                                    perror(“Error in Accepting”);
+
+                                    exit(-1);
+
+                        }
+
+                         sentconf =  send(cli, mesg, strlen(mesg), 0);
+
+                        printf(“Sending %d bytes to clientDetail : %c\n”, sentconf,                                                                 inet_ntoa(clientDetail.sin_addr));
+
+                        close(cli);
+
+            }
+
 }
