7ddb213eec5c19664c4fdbc10d5d040f6e3a85d6
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 08:01:57 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
-
-int main(void)
+#include <stdlib.h>
+#include <string.h>
+#include<winsock.h>
+#include<conio.h>
+ 
+#define MAXRCVLEN 500
+#define PORTNUM 2343
+ 
+int main(int argc, char *argv[])
 {
-    printf("Hello!\n");
+    char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
+    int len, mysocket;
+    struct sockaddr_in dest; 
+ 
+    mysocket = socket(AF_INET, SOCK_STREAM, 0);
+  
+    memset(&dest, 0, sizeof(dest));                /* zero the struct */
+    dest.sin_family = AF_INET;
+    dest.sin_addr.s_addr = inet_addr("127.0.0.1"); /* set destination IP number */
+    dest.sin_port = htons(PORTNUM);                /* set destination port number */
+ 
+    connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+  
+    len = recv(mysocket, buffer, MAXRCVLEN, 0);
+ 
+    /* We have to null terminate the received data ourselves */
+    buffer[len] = '\0';
+ 
+    printf("Received %s (%d bytes).\n", buffer, len);
+ 
+    closesocket(mysocket);
+    getch();
+    return EXIT_SUCCESS;
 }
