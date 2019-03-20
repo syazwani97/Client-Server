4e53cf4ae7e2c47455d74bf12c20188af97ade59
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 08:48:55 +0800

    Update client.c

--- client.c
+++ client.c
@@ -11,10 +11,10 @@
 int main(int argc , char const *argv[])
 {
      int sockfd = 0, vread;
-     char *annyeong = "annyeong from client";
+     char *hello = "hello from client";
      char buff[1000] = {0};
      struct sockaddr_in adrress;
-     struct sockaddr_in servaddr;
+     struct sockaddr_in serv_addr;
      
      //create socket
      if ((sockfd = socket(AF_INET, SOCK_STREAM,0)) < 0)
@@ -22,12 +22,12 @@ int main(int argc , char const *argv[])
           printf ("could not create socket");
 	  return -1;
      }
-     memset(&servaddr, '0', sizeof(servaddr));
+     memset(&serv_addr, '0', sizeof(serv_addr));
      
-     servaddr.sin_family = AF_INET;
-     servaddr.sin_port = htons(5000);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_port = htons(5000);
      
-     if (inet_pton(AF_INET, "192.168.216.128", &servaddr.sin_addr) <= 0)
+     if (inet_pton(AF_INET, "192.168.184.131", &serv_addr.sin_addr) <= 0)
      {
           //print the error message
           printf("\n it's error \n");
@@ -35,13 +35,12 @@ int main(int argc , char const *argv[])
      }
 	
 	//Accept and incoming connection
-	send(sockfd, annyeong, strlen(annyeong) , 0);
-	  printf("annyeong message sent");
+	send(sockfd, hello, strlen(hello) , 0);
+	  printf("hello message sent");
 	  vread = read(sockfd, buff , 1000);
 	  printf("%s\n" , buff);
 	  
 	
 	return 0;
 }
-
 
