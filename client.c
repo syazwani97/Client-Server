79cb018e386860b688063d35d9659eb6cf62a050
syazwani97 <48686663+syazwani97@users.noreply.github.com>
2019-03-20 08:31:16 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,91 +1,68 @@
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-#include<stdio.h>
+//#define ServerIP "35.162.226.229"
+#define ServerIP "127.0.0.1"
+#define ServerPort 7777
+#define SIZE 1024
 
-#include<stdlib.h>
 
-#include<sys/types.h>
 
-#include<sys/socket.h>
 
-#include<netinet/in.h>
 
-#include<errno.h>
+int main(int argc, char** argv) {
 
-main()
+    int socket_fd;
+    struct sockaddr_in serverAddr;
+    char text[SIZE],msg[SIZE-20], name[20]text temizlendi,get_msg[1024];
+    int byte;
+    // Nick
+    printf("Please enter your nick"); 
+    scanf("%s",name);
+    //Create Socket
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_fd == -1)
+        perror("Error on Socket");
 
-{
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(ServerPort);
+    serverAddr.sin_addr.s_addr =inet_addr(ServerIP);
+    memset(&(serverAddr.sin_zero), '\0', 8);
 
-            int sock, cli;
+    // Connect to server
+    if(connect(socket_fd, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr)) == -1)
+        perror("Error on Connect");
+    puts("Connected");
+    while(1)
+    {
+        //Get message
+        printf(">>>"); scanf("%s",msg);
+        //Edit message
+        strcpy(text, name);
+        strcat(text, " : ");
+        strcat(text, msg);
+        //Send message
+        byte = send(socket_fd, text, strlen(text), 0);
+        if(byte == -1)
+            perror("Error on Send");
+        else if(byte == 0)
+            printf("Connection've been closed");
+        //Get reply to other users
 
-            struct sockaddr_in server, clientDetail;
+        recv(socket_fd, &get_msg, SIZE-1, 0);
+        printf("Getting message %s", get_msg);
+    }
+    //Close socket
+    close(socket_fd);
 
-            unsigned int len;
 
-            char mesg[] = “Hello world with Socket programming!”;
 
-            int sentconf;
-
-             if((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-
-            {
-
-                        perror(“There are some issue in creating socket : “);
-
-                        exit(-1);
-
-            }
-
-             server.sin_family = AF_INET;
-
-            server.sin_port = htons(10012);
-
-            server.sin_addr.s_addr = INADDR_ANY;
-
-            bzero(&server.sin_zero, 8);
-
-             len = sizeof(struct sockaddr_in);
-
-             if((bind(sock, (struct sockaddr *)&server, len)) == -1)
-
-            {
-
-                        perror(“Error in Binding”);
-
-                        exit(-1);
-
-            }
-
-             if((listen(sock, 5)) == -1)
-
-            {
-
-                        perror(“Error in Listening”);
-
-                        exit(-1);
-
-            }
-
-            while(1)
-
-            {
-
-                        if((cli = accept(sock, (struct sockaddr *)&clientDetail, &len)) ==-1)
-
-                        {
-
-                                    perror(“Error in Accepting”);
-
-                                    exit(-1);
-
-                        }
-
-                         sentconf =  send(cli, mesg, strlen(mesg), 0);
-
-                        printf(“Sending %d bytes to clientDetail : %c\n”, sentconf,                                                                 inet_ntoa(clientDetail.sin_addr));
-
-                        close(cli);
-
-            }
-
+    return (EXIT_SUCCESS);
 }
